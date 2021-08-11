b32c31c7788c1371fa5bbfe6da7b891519dbd16b
Dave Bray <david@brayworth.com.au>
2021-08-11 20:59:36 +1000

    dev

--- run.sh
+++ run.sh
@@ -2,10 +2,68 @@
 
 WD=`pwd`
 PORT=$[RANDOM%10000+1024]
-php=php
-if [[ -x /usr/bin/php8 ]]; then php=php8; fi
 
-cd www
-echo "this application is available at http://localhost:$PORT"
-$php -S localhost:$PORT _mvp.php
-cd $WD
+apache=`command -v httpd`
+
+
+if [[ "" == $apache ]]; then
+  cd www
+
+  php=php
+  if [[ -x /usr/bin/php8 ]]; then php=php8; fi
+
+  echo "this application is available at http://localhost:$PORT"
+  $php -S localhost:$PORT _mvp.php
+
+  cd $WD
+
+else
+  data="`pwd`/src/app/data"
+  error_log="$data/error.log"
+  access_log="$data/access.log"
+  config="$data/httpd.conf"
+  pidFile="$data/httpd.pid"
+
+  if [ "$1" == "kill" ]; then
+    if [[ -f $pidFile ]]; then
+      kill `cat $pidFile`
+      if [[ -f $pidFile ]]; then
+        rm $pidFile
+
+      fi
+
+    fi
+
+  else
+
+    [[ ! -f $error_log ]] || rm $error_log
+    [[ ! -f $access_log ]] || rm $access_log
+    if [[ ! -f $config ]]; then
+      cp $WD/httpd-minimal.conf $config
+      echo "ErrorLogFormat \"[%t] %M\"" >>$config
+      echo "Listen $PORT" >>$config
+      echo "ErrorLog $error_log" >>$config
+      echo "CustomLog $access_log common" >>$config
+      echo "DocumentRoot `pwd`/www" >>$config
+      echo "<Directory `pwd`/www>" >>$config
+      echo "  AllowOverride all" >>$config
+      echo "  Require all granted" >>$config
+      echo "</Directory>" >>$config
+
+    fi
+
+    if [[ -f $pidFile ]] ; then
+      echo "running ..`cat $pidFile`"
+
+    else
+      echo "this application is available at http://localhost:$PORT"
+      httpd \
+        -f $config \
+        -c "PidFile $pidFile"
+      tail -f src/dev/data/error.log
+
+    fi
+
+  fi
+
+fi
--- src/forum/views/view-comment-form-2.php
+++ src/forum/views/view-comment-form-2.php
@@ -9,99 +9,109 @@
 */	?>
 
 <div id="commentForm" class="d-print-none" data-role="comment-container" data-master="yes">
-  <div class="row">
-    <div class="offset-lg-1 offset-md-2 col pl-md-0">
-      <i id="show-comment-box" class="bi bi-caret-right"></i>
-      Add your Comment
+	<div class="row">
+		<div class="offset-lg-1 offset-md-2 col pl-md-0">
+			<i id="show-comment-box" class="bi bi-caret-right"></i>
+			Add your Comment
 
-    </div>
+		</div>
 
-  </div>
+	</div>
 
-  <div class="row d-none" data-comment-body>
-    <div class="offset-lg-1 offset-md-2 col pl-md-0">
-      <input type="hidden" name="parent" value="<?= $this->data->dto->id ?>" />
-      <input type="hidden" id="commentThread" name="thread" data-default="<?= $this->data->dto->id ?>" value="<?= $this->data->dto->id ?>" />
+	<div class="row d-none" data-comment-body>
+		<div class="offset-lg-1 offset-md-2 col pl-md-0">
+			<input type="hidden" name="parent" value="<?= $this->data->dto->id ?>" />
+			<input type="hidden" id="commentThread" name="thread" data-default="<?= $this->data->dto->id ?>" value="<?= $this->data->dto->id ?>" />
 
-      <div class="form-group row">
-        <div class="col">
-          <textarea data-role="comment" name="comment" id="<?= $uid = uniqid('cms_') ?>"
-            class="form-control"
-            rows="10" placeholder="add your comment"></textarea>
+			<div class="form-group row">
+				<div class="col">
+					<textarea data-role="comment" name="comment" id="<?= $uid = uniqid('cms_') ?>" class="form-control" rows="10" placeholder="add your comment"></textarea>
 
-        </div>
+				</div>
 
-      </div>
+			</div>
 
-      <div class="form-group row">
-        <div class="col">
-          <input class="btn btn-primary" type="submit" name="form_action" id="<?= $uid ?>form_button" value="post comment" />
-          <?php if ( currentUser::isDavid()) {	?>
-          <button class="btn btn-outline-primary" type="button" id="<?= $uid ?>dvmc">Done, verify and mark complete</button>
+			<div class="form-group row">
+				<div class="col">
+					<input class="btn btn-primary" type="submit" name="form_action" id="<?= $uid ?>form_button" value="post comment" />
+					<?php if (currentUser::isDavid()) {	?>
+						<button class="btn btn-outline-primary" type="button" id="<?= $uid ?>dvmc">Done, verify and mark complete</button>
 
-          <?php }	?>
+					<?php }	?>
 
-        </div>
+				</div>
 
-      </div>
+			</div>
 
-    </div>
+		</div>
 
-  </div>
+	</div>
 
 </div>
 <script>
-( _ => $(document).ready( () => {
-	let editorDone = false;
+	(_ => $(document).ready(() => {
+		let editorDone = false;
 
-	$('#show-comment-box').parent().addClass('pointer').on( 'click', function( e) {
-		e.stopPropagation(); e.preventDefault();
+		$('#show-comment-box').parent().addClass('pointer').on('click', function(e) {
+			e.stopPropagation();
+			e.preventDefault();
 
-		var _el = $('#show-comment-box', this);
+			var _el = $('#show-comment-box', this);
 
-		if ( _el.data('state') == 'open') {
-			_el.removeClass('bi-caret-down').addClass('bi-caret-right');
-			_el.data('state', 'hidden');
-			$( '#commentForm [data-comment-body]').addClass('d-none');
+			if (_el.data('state') == 'open') {
+				_el.removeClass('bi-caret-down').addClass('bi-caret-right');
+				_el.data('state', 'hidden');
+				$('#commentForm [data-comment-body]').addClass('d-none');
 
-		}
-		else {
-			_el.removeClass('bi-caret-right').addClass('bi-caret-down');
-			_el.data('state', 'open');
-			$( '#commentForm [data-comment-body]').removeClass('d-none');
+			} else {
+				_el.removeClass('bi-caret-right').addClass('bi-caret-down');
+				_el.data('state', 'open');
+				$('#commentForm [data-comment-body]').removeClass('d-none');
 
-			if ( !editorDone) {
-				editorDone = true;
+				if (!editorDone) {
+					editorDone = true;
 
-				_.tiny().then( () => {
-					tinymce.init({
-						browser_spellcheck : true,
-						document_base_url : _.url('',true),
-						menubar : false,
-						paste_data_images: true,
-						plugins: 'paste imagetools table autolink lists',
-						selector: '#<?= $uid ?>',
-						statusbar : false,
-						toolbar: 'undo redo | bold italic | bullist numlist outdent indent blockquote',
+					_.tiny().then(() => {
+						let j = {
+							browser_spellcheck: true,
+							document_base_url: _.url('', true),
+							menubar: false,
+							paste_data_images: true,
+							plugins: 'paste imagetools table autolink lists',
+							selector: '#<?= $uid ?>',
+							statusbar: false,
+							toolbar: 'undo redo done | bold italic | bullist numlist outdent indent blockquote',
+
+						};
+
+						j.setup = ed => {
+							ed.ui.registry.addButton('done', {
+								tooltip: 'all done',
+								text: '&check;',
+								onAction: () => ed.insertContent('done, verify and mark complete')
+
+							})
+						};
+
+						tinymce.init(j);
 
 					});
 
-				});
+				}
 
 			}
 
-		}
+		});
 
-	});
+		$('#<?= $uid ?>dvmc')
+			.on('click', function(e) {
+				//~ tinymce.EditorManager.execCommand('mceRemoveEditor',true, editor_id);
+				tinyMCE.execCommand('mceRemoveEditor', false, '<?= $uid ?>');
 
-	$('#<?= $uid ?>dvmc').on( 'click', function( e) {
-		//~ tinymce.EditorManager.execCommand('mceRemoveEditor',true, editor_id);
-		tinyMCE.execCommand('mceRemoveEditor', false, '<?= $uid ?>');
+				// $('#<?= $uid ?>').val('Done, verify and mark complete');
+				$('#<?= $uid ?>form_button').click();
 
-		$('#<?= $uid ?>').val('Done, verify and mark complete');
-		$('#<?= $uid ?>form_button').click();
+			});
 
-	});
-
-}))( _brayworth_);
+	}))(_brayworth_);
 </script>
