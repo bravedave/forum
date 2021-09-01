75322e773e192dd4435f5420c3ad5e219810bc97
Dave Bray <david@brayworth.com.au>
2021-09-01 10:17:48 +1000

    bug

--- src/forum/views/new-post.php
+++ src/forum/views/new-post.php
@@ -40,8 +40,7 @@ use dvc\bs;
                 <input type="text" class="form-control" name="tag" id="<?= $_tag = strings::rand() ?>" placeholder="tag" required>
 
                 <div class="input-group-append">
-                  <button class="btn btn-light dropdown-toggle" type="button" <?= bs::data('toggle', 'dropdown') ?>
-                    aria-haspopup="true" aria-expanded="false"></button>
+                  <button class="btn btn-light dropdown-toggle" type="button" <?= bs::data('toggle', 'dropdown') ?> aria-haspopup="true" aria-expanded="false"></button>
                   <div class="dropdown-menu dropdown-menu-right" id="<?= $_tags = strings::rand() ?>"></div>
 
                 </div>
@@ -49,27 +48,26 @@ use dvc\bs;
               </div>
 
               <script>
-                ( _ => {
-                  $(<?= json_encode( $this->data->tags) ?>).each( (i, tag) => {
+                (_ => {
+                  $(<?= json_encode($this->data->tags) ?>).each((i, tag) => {
                     let a = $('<a class="dropdown-item" href="#"></a');
 
                     a
-                    .html( tag.tag)
-                    .on( 'click', function( e) {
-                      $('#<?= $_tag ?>').val( this.innerHTML);
+                      .html(tag.tag)
+                      .on('click', function(e) {
+                        $('#<?= $_tag ?>').val(this.innerHTML);
 
-                      if ( !_brayworth_.browser.isMobileDevice) {
-                        $('#<?= $_tag ?>').focus().select();
+                        if (!_brayworth_.browser.isMobileDevice) {
+                          $('#<?= $_tag ?>').focus().select();
 
-                      }
+                        }
 
-                    })
-                    .appendTo( '#<?= $_tags ?>');
+                      })
+                      .appendTo('#<?= $_tags ?>');
 
                   });
 
-                }) (_brayworth_);
-
+                })(_brayworth_);
               </script>
 
             </div>
@@ -108,52 +106,51 @@ use dvc\bs;
                   <option value="">select person to notify</option>
                 </select>
                 <script>
-                ( _ => {
-                  _.post({
-                    url : _.url('<?= $this->route ?>'),
-                    data : {
-                      action : 'get-active-users'
+                  (_ => {
+                    _.post({
+                      url: _.url('<?= $this->route ?>'),
+                      data: {
+                        action: 'get-active-users'
 
-                    },
+                      },
 
-                  }).then( d => {
-                    if ( 'ack' == d.response) {
-                      $.each( d, (i,u) => {
-                        $('#<?= $_uid ?>').append(  $('<option></option>').val( u.email).html( u.email));
+                    }).then(d => {
+                      if ('ack' == d.response) {
+                        $.each(d.data, (i, u) => {
+                          $('#<?= $_uid ?>').append($('<option></option>').val(u.email).html(u.email));
 
-                      });
+                        });
 
-                      $('#<?= $_uid ?>').on( 'change', function( e) {
-                        if ( this.selectedIndex > 0) {
+                        $('#<?= $_uid ?>').on('change', function(e) {
+                          if (this.selectedIndex > 0) {
 
-                          let _me = $(this);
+                            let _me = $(this);
 
-                          let col = $('<div class="col"></div>').prependTo( _me.closest('.form-row'));
-                          $('<input type="hidden" name="notify[]">').val( $(this).val()).appendTo( col);
+                            let col = $('<div class="col"></div>').prependTo(_me.closest('.form-row'));
+                            $('<input type="hidden" name="notify[]">').val($(this).val()).appendTo(col);
 
-                          let ig = $( '<div class="input-group"></div>').appendTo( col);
-                          $('<input type="text" class="form-control">').val( $(this).val()).appendTo( ig);
-                          $('<div class="input-group-append pointer"><button type="button" class="btn input-group-text">&times;</button></div>')
-                          .appendTo( ig)
-                          .on( 'click', e => {
-                            e.stopPropagation();;
-                            col.remove();
+                            let ig = $('<div class="input-group"></div>').appendTo(col);
+                            $('<input type="text" class="form-control">').val($(this).val()).appendTo(ig);
+                            $('<div class="input-group-append pointer"><button type="button" class="btn input-group-text">&times;</button></div>')
+                              .appendTo(ig)
+                              .on('click', e => {
+                                e.stopPropagation();;
+                                col.remove();
 
-                          });
+                              });
 
-                        }
+                          }
 
-                      });
+                        });
 
-                    }
-                    else {
-                      _.growl( d);
+                      } else {
+                        _.growl(d);
 
-                    }
+                      }
 
-                  });
+                    });
 
-                }) (_brayworth_);
+                  })(_brayworth_);
                 </script>
 
               </div>
@@ -177,121 +174,120 @@ use dvc\bs;
   </div>
 
   <script>
-  ( _ => $(document).ready( () => {
-    $('#<?= $_modal ?>')
-    .on( 'init-tinymce', e => {
-      // inline: true,
-      let options = {
-        browser_spellcheck : true,
-        font_formats: "Andale Mono=andale mono,times;"+
-          "Arial=arial,helvetica,sans-serif;"+
-          "Arial Black=arial black,avant garde;"+
-          "Century Gothic=century gothic,arial,helvetica,sans-serif;"+
-          "Comic Sans MS=comic sans ms,sans-serif;"+
-          "Courier New=courier new,courier;"+
-          "Helvetica=helvetica;"+
-          "Impact=impact,chicago;"+
-          "Symbol=symbol;"+
-          "Tahoma=tahoma,arial,helvetica,sans-serif;"+
-          "Terminal=terminal,monaco;"+
-          "Times New Roman=times new roman,times;"+
-          "Trebuchet MS=trebuchet ms,geneva;"+
-          "Verdana=verdana,geneva;"+
-          "Webdings=webdings;"+
-          "Wingdings=wingdings,zapf dingbats",
-        branding: false,
-        document_base_url : _.url('',true),
-        menubar : false,
-        selector: '#<?= $_uidComment ?>',
-        paste_data_images: true,
-        relative_urls : false,
-        remove_script_host : false,
-        setup : ed => {
-          ed.on( 'keydown', e => {
-            if (e.keyCode == 9) { // tab pressed
-              if (e.shiftKey)
-                ed.execCommand('Outdent');
-              else
-                ed.execCommand('Indent');
+    (_ => $(document).ready(() => {
+      $('#<?= $_modal ?>')
+        .on('init-tinymce', e => {
+          // inline: true,
+          let options = {
+            browser_spellcheck: true,
+            font_formats: "Andale Mono=andale mono,times;" +
+              "Arial=arial,helvetica,sans-serif;" +
+              "Arial Black=arial black,avant garde;" +
+              "Century Gothic=century gothic,arial,helvetica,sans-serif;" +
+              "Comic Sans MS=comic sans ms,sans-serif;" +
+              "Courier New=courier new,courier;" +
+              "Helvetica=helvetica;" +
+              "Impact=impact,chicago;" +
+              "Symbol=symbol;" +
+              "Tahoma=tahoma,arial,helvetica,sans-serif;" +
+              "Terminal=terminal,monaco;" +
+              "Times New Roman=times new roman,times;" +
+              "Trebuchet MS=trebuchet ms,geneva;" +
+              "Verdana=verdana,geneva;" +
+              "Webdings=webdings;" +
+              "Wingdings=wingdings,zapf dingbats",
+            branding: false,
+            document_base_url: _.url('', true),
+            menubar: false,
+            selector: '#<?= $_uidComment ?>',
+            paste_data_images: true,
+            relative_urls: false,
+            remove_script_host: false,
+            setup: ed => {
+              ed.on('keydown', e => {
+                if (e.keyCode == 9) { // tab pressed
+                  if (e.shiftKey)
+                    ed.execCommand('Outdent');
+                  else
+                    ed.execCommand('Indent');
 
-              e.preventDefault();
-              return false;
+                  e.preventDefault();
+                  return false;
+
+                }
+
+              });
+
+              ed.on('init', e => _.hourglass.off());
+              ed.on('blur', e => tinyMCE.triggerSave());
 
             }
 
+          };
+
+          options = _.extend(options, {
+            plugins: [
+              'paste',
+              'imagetools',
+              'table',
+              'autolink',
+              'lists',
+              'link',
+
+            ],
+            statusbar: false,
+            toolbar: 'undo redo | bold italic | bullist numlist outdent indent blockquote table link mybutton | styleselect fontselect fontsizeselect | forecolor backcolor',
+            contextmenu: 'paste | inserttable | cell row column deletetable',
+
           });
 
-          ed.on( 'init', e => _.hourglass.off());
-          ed.on( 'blur', e => tinyMCE.triggerSave());
+          tinymce.init(options);
 
-        }
+        })
+        .on('shown.bs.modal', e => {
+          _.hourglass.on();
+          _.tiny().then(() => $('#<?= $_modal ?>').trigger('init-tinymce'));
 
-      };
+        });
 
-      options = _.extend( options, {
-        plugins: [
-          'paste',
-          'imagetools',
-          'table',
-          'autolink',
-          'lists',
-          'link',
+      $('#<?= $_form ?>')
+        .on('submit', function(e) {
+          tinyMCE.triggerSave();
 
-        ],
-        statusbar : false,
-        toolbar: 'undo redo | bold italic | bullist numlist outdent indent blockquote table link mybutton | styleselect fontselect fontsizeselect | forecolor backcolor',
-        contextmenu: 'paste | inserttable | cell row column deletetable',
+          let _form = $(this);
+          let _data = _form.serializeFormJSON();
 
-      });
+          $('#<?= $_modal ?> .modal-footer .alert').remove();
 
-      tinymce.init(options);
-
-    })
-    .on( 'shown.bs.modal', e => {
-      _.hourglass.on();
-      _.tiny().then( () => $('#<?= $_modal ?>').trigger('init-tinymce'));
-
-    });
-
-    $('#<?= $_form ?>')
-    .on( 'submit', function( e) {
-      tinyMCE.triggerSave();
-
-      let _form = $(this);
-      let _data = _form.serializeFormJSON();
-
-      $('#<?= $_modal ?> .modal-footer .alert').remove();
-
-      if ( '' == _data.comment) {
-        $('#<?= $_modal ?> .modal-footer').prepend( '<div class="alert alert-warning mr-auto">please enter description</div>');
-        return false;
-
-      }
-
-      // console.log( _data);
-      _.post({
-        url : _.url('<?= $this->route ?>'),
-        data : _data,
-
-      }).then( d => {
-          if ( 'ack' == d.response) {
-            $('#<?= $_modal ?>').trigger( 'success', _data);
-
-          }
-          else {
-            _.growl(d);
+          if ('' == _data.comment) {
+            $('#<?= $_modal ?> .modal-footer').prepend('<div class="alert alert-warning mr-auto">please enter description</div>');
+            return false;
 
           }
 
-          $('#<?= $_modal ?>').modal( 'hide');
+          // console.log( _data);
+          _.post({
+            url: _.url('<?= $this->route ?>'),
+            data: _data,
 
-      });
+          }).then(d => {
+            if ('ack' == d.response) {
+              $('#<?= $_modal ?>').trigger('success', _data);
 
-      return false;
+            } else {
+              _.growl(d);
 
-    });
+            }
 
-  }))( _brayworth_);
+            $('#<?= $_modal ?>').modal('hide');
+
+          });
+
+          return false;
+
+        });
+
+    }))(_brayworth_);
   </script>
 
 </form>
