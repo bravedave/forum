3e2c1d540c6f5b3866396d1c55be9f334eccbdbe
Dave Bray <david@brayworth.com.au>
2024-07-22 18:32:00 +1000

    incompatible plugins on mobile

--- src/forum/views/view-comment-form.php
+++ src/forum/views/view-comment-form.php
@@ -27,88 +27,96 @@ extract((array)$this->data);
 
 <input type="hidden" name="form_action" value="post comment">
 <div id="commentForm" class="d-print-none" data-role="comment-container" data-master="yes">
-	<div class="row g-2">
-		<div class="col ps-md-0">
-			<i id="<?= $_uidShowCommentBox = strings::rand() ?>" class="bi bi-caret-right"></i>
-			Add your Comment
-		</div>
-	</div>
+  <div class="row g-2">
+    <div class="col ps-md-0">
+      <i id="<?= $_uidShowCommentBox = strings::rand() ?>" class="bi bi-caret-right"></i>
+      Add your Comment
+    </div>
+  </div>
 
-	<div class="row g-2 d-none" data-comment-body>
+  <div class="row g-2 d-none" data-comment-body>
 
-		<div class="col ps-md-0">
+    <div class="col ps-md-0">
 
-			<input type="hidden" name="parent" value="<?= $dto->id ?>" />
-			<input type="hidden" id="commentThread" name="thread" data-default="<?= $dto->id ?>" value="<?= $dto->id ?>" />
+      <input type="hidden" name="parent" value="<?= $dto->id ?>" />
+      <input type="hidden" id="commentThread" name="thread" data-default="<?= $dto->id ?>"
+        value="<?= $dto->id ?>" />
 
-			<div class="mb-2">
+      <div class="mb-2">
 
-				<textarea data-role="comment" name="comment" id="<?= $uid = strings::rand() ?>" class="form-control" rows="16" placeholder="add your comment"></textarea>
-			</div>
+        <textarea data-role="comment" name="comment" id="<?= $uid = strings::rand() ?>" class="form-control" rows="16"
+          placeholder="add your comment"></textarea>
+      </div>
 
-			<button type="submit" class="btn btn-primary" id="<?= $uid ?>form_button">post comment</button>
+      <button type="submit" class="btn btn-primary" id="<?= $uid ?>form_button">post comment</button>
 
-			use <span class="user-select-all fw-bold">{topic:&lt;id&gt;}</span> to reference a topic
-		</div>
-	</div>
+      use <span class="user-select-all fw-bold">{topic:&lt;id&gt;}</span> to reference a topic
+    </div>
+  </div>
 </div>
 <script>
-	(_ => {
-		let editorDone = false;
+  (_ => {
+    let editorDone = false;
 
-		$('#<?= $_uidShowCommentBox ?>')
-			.parent()
-			.addClass('pointer')
-			.on('click', function(e) {
-				e.stopPropagation();
-				e.preventDefault();
+    $('#<?= $_uidShowCommentBox ?>')
+      .parent()
+      .addClass('pointer')
+      .on('click', function(e) {
+        e.stopPropagation();
+        e.preventDefault();
 
-				let _el = $('#<?= $_uidShowCommentBox ?>');
+        let _el = $('#<?= $_uidShowCommentBox ?>');
 
-				if (_el.data('state') == 'open') {
+        if (_el.data('state') == 'open') {
 
-					_el
-						.removeClass('bi-caret-down')
-						.addClass('bi-caret-right')
-						.data('state', 'hidden');
-					$('#commentForm [data-comment-body]').addClass('d-none');
-				} else {
+          _el
+            .removeClass('bi-caret-down')
+            .addClass('bi-caret-right')
+            .data('state', 'hidden');
+          $('#commentForm [data-comment-body]').addClass('d-none');
+        } else {
 
-					_el
-						.removeClass('bi-caret-right')
-						.addClass('bi-caret-down')
-						.data('state', 'open');
-					$('#commentForm [data-comment-body]').removeClass('d-none');
+          _el
+            .removeClass('bi-caret-right')
+            .addClass('bi-caret-down')
+            .data('state', 'open');
+          $('#commentForm [data-comment-body]').removeClass('d-none');
 
-					if (!editorDone) {
-						editorDone = true;
+          if (!editorDone) {
+            editorDone = true;
 
-						_.tiny().then(() => {
-							let j = {
-								browser_spellcheck: true,
-								deprecation_warnings: false,
-								document_base_url: _.url('', true),
-								menubar: false,
-								paste_data_images: true,
-								plugins: 'paste imagetools table autolink lists link',
-								selector: '#<?= $uid ?>',
-								statusbar: false,
-								toolbar: 'undo redo | done | bold italic | bullist numlist outdent indent blockquote link',
-							};
+            _.tiny().then(() => {
+              let j = {
+                browser_spellcheck: true,
+                deprecation_warnings: false,
+                document_base_url: _.url('', true),
+                menubar: false,
+                paste_data_images: true,
+                plugins: 'paste imagetools table autolink lists link',
+                selector: '#<?= $uid ?>',
+                statusbar: false,
+                toolbar: 'undo redo | done | bold italic | bullist numlist outdent indent blockquote link',
+              };
 
-							j.setup = ed => {
+              if (_.browser.isMobileDevice) {
 
-								ed.ui.registry.addButton('done', {
-									tooltip: 'all done',
-									text: '&check;',
-									onAction: () => ed.insertContent('done, verify and mark complete')
-								})
-							};
+                // https://cmss.darcy.com.au/forum/view/11879
+                j.plugins = 'lists autolink';
+              }
 
-							tinymce.init(j);
-						});
-					}
-				}
-			});
-	})(_brayworth_);
+              j.setup = ed => {
+
+                ed.ui.registry.addButton('done', {
+                  tooltip: 'all done',
+                  text: '&check;',
+                  onAction: () => ed.insertContent('done, verify and mark complete')
+                })
+              };
+
+              tinymce.init(j);
+            });
+          }
+        }
+      });
+  })(_brayworth_);
 </script>
